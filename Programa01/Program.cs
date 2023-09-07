59d2e8ad58864d3d8a1deece7998457d1a5099b3
Felipe <Felipe@DESKTOP-BESFNFE>
2023-09-07 14:03:19 -0300

    Adicionar arquivos de projeto.

---
+++ Programa01/Program.cs
@@ -0,0 +1,35 @@
+﻿//Random Number
+string texto_personalizado = @"
+█▀█ ▄▀█ █▄░█ █▀▄ █▀█ █▀▄▀█   █▄░█ █░█ █▀▄▀█ █▄▄ █▀▀ █▀█
+█▀▄ █▀█ █░▀█ █▄▀ █▄█ █░▀░█   █░▀█ █▄█ █░▀░█ █▄█ ██▄ █▀▄
+";
+
+
+void inicio()
+{
+    Console.WriteLine(texto_personalizado);
+    Console.WriteLine("Bem vindo! , tente acertar o número correto.(Entre 0 e 100)");
+}
+
+inicio();
+Random aleatorio = new Random();
+int numGerado = aleatorio.Next(1, 101);
+//Console.WriteLine($"Resposta:{numGerado}");
+do
+{
+    Console.Write("Faça seu chute:");
+    int chute = int.Parse(Console.ReadLine()!);
+    if (chute > numGerado)
+    {
+        Console.WriteLine("O número é menor que este valor!");
+    }
+    else if (chute < numGerado)
+    {
+        Console.WriteLine("O número é maior que este valor!");
+    }
+    else
+    {
+        Console.WriteLine("Parabéns você acertou!");
+        break;
+    }
+} while (true);
